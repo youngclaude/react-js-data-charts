334f4e2f01d25d393e10b4e41846c62fb38c34bb
Rene CLaude <rclaude@you42.com>
2019-08-02 18:32:29 -0400

    Initializing docs folder for GithubPages

---
+++ docs/precache-manifest.35874d9c819fc236ad0968b9346349d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34ad749b075dfb743fed07e99605f801",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73c179b49574353fa8c4",
+    "url": "/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "4d28dfdbc825b68e1258",
+    "url": "/static/js/2.f332bf17.chunk.js"
+  },
+  {
+    "revision": "73c179b49574353fa8c4",
+    "url": "/static/js/main.44d73e04.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
