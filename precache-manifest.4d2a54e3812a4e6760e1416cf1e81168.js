a19509c32deaa0b4cd65d3f1dc8bcc3631b23083
youngclaude <clauderene@ymail.com>
2021-12-09 17:28:52 -0500

    Updates

---
+++ precache-manifest.4d2a54e3812a4e6760e1416cf1e81168.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef3544be3c5093f847e9ded49cceac6c",
+    "url": "/react-js-data-charts/index.html"
+  },
+  {
+    "revision": "0d3f94fcf35aa3070878",
+    "url": "/react-js-data-charts/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "356e858553007c7da1aa",
+    "url": "/react-js-data-charts/static/js/2.7b4e9b2f.chunk.js"
+  },
+  {
+    "revision": "0d3f94fcf35aa3070878",
+    "url": "/react-js-data-charts/static/js/main.9bd24569.chunk.js"
+  },
+  {
+    "revision": "23bf7e88781d93dff4a7",
+    "url": "/react-js-data-charts/static/js/runtime~main.06350a94.js"
+  }
+]);
